80d3a707d644121802f386396da7c4a341c75972
Shirataki2 <ahahah114514@gmail.com>
2018-03-08 23:05:30 +0900

    '進捗'

---
+++ Chapter4/RandomForest.py
@@ -0,0 +1,20 @@
+from Wine import getWineRawData, getWineData
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+import numpy as np
+
+X_train, X_test, y_train, y_test = getWineData(kind='none')
+df_wine = getWineRawData()
+
+feat_labels = df_wine.columns[1:]
+forest = RandomForestClassifier(n_estimators=10000, random_state=0, n_jobs=-1)
+forest.fit(X_train, y_train)
+importances = forest.feature_importances_
+indices = np.argsort(importances)[::-1]
+plt.title('Feature Importances')
+plt.bar(range(X_train.shape[1]), importances[indices],
+        color='lightblue', align='center')
+plt.xticks(range(X_train.shape[1]), feat_labels[indices], rotation=90)
+plt.xlim([-1, X_train.shape[1]])
+plt.tight_layout()
+plt.show()
--- Chapter4/SBS.py
+++ Chapter4/SBS.py
@@ -1,8 +1,11 @@
 from sklearn.base import clone
 from itertools import combinations
-import numpy as np
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from Wine import getWineData
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 class SBS(object):
@@ -19,3 +22,45 @@ class SBS(object):
         X_train, X_test, y_train, y_test = train_test_split(
             X, y, test_size=self.test_size, random_state=self.random_state)
         dim = X_train.shape[1]
+        self.indices_ = tuple(range(dim))
+        self.subsets_ = [self.indices_]
+        score = self._calc_score(
+            X_train, y_train, X_test, y_test, self.indices_)
+        self.scores_ = [score]
+        while dim > self.k_features:
+            scores = []
+            subsets = []
+            for p in combinations(self.indices_, r=dim-1):
+                score = self._calc_score(X_train, y_train, X_test, y_test, p)
+                scores.append(score)
+                subsets.append(p)
+            best = np.argmax(scores)
+            self.indices_ = subsets[best]
+            self.subsets_.append(self.indices_)
+            dim -= 1
+            self.scores_.append(scores[best])
+        self.k_score_ = self.scores_[-1]
+        return self
+
+    def tranform(self, X):
+        return X[:, self.indices_]
+
+    def _calc_score(self, X_train, y_train, X_test, y_test, indices):
+        self.estimator.fit(X_train[:, indices], y_train)
+        y_pred = self.estimator.predict(X_test[:, indices])
+        score = self.scoring(y_test, y_pred)
+        return score
+
+
+if __name__ == '__main__':
+    X_train_std, X_test_std, y_train, y_test = getWineData()
+    knn = KNeighborsClassifier(n_neighbors=2)
+    sbs = SBS(knn, k_features=1)
+    sbs.fit(X_train_std, y_train)
+    k_feat = [len(k) for k in sbs.subsets_]
+    plt.plot(k_feat, sbs.scores_, marker='o')
+    plt.ylim([0.7, 1.1])
+    plt.ylabel('Accuracy')
+    plt.xlabel('Number of feature')
+    plt.grid()
+    plt.show()
